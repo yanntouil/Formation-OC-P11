d3d3c83b19addcf010ec79b71e20dc5c67e5f6df
Yann Touil <yanntouil@gmail.com>
2021-11-10 09:02:35 +0100

    Add GH page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Component } from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import AccommodationsContextProvider from './Context/AccommodationsContext';
-import Header from './Components/Layout/Header';
-import Footer from './Components/Layout/Footer';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'//BrowserRouter
+import AccommodationsContextProvider from './Context/AccommodationsContext'
+import Header from './Components/Layout/Header'
+import Footer from './Components/Layout/Footer'
 import Home from './Pages/Home.js'
-import Accommodation from './Pages/Accommodation';
-import AboutUs from './Pages/AboutUs.js';
-import Error404 from './Pages/Error404.js';
-import './App.scss';
+import Accommodation from './Pages/Accommodation'
+import AboutUs from './Pages/AboutUs.js'
+import Error404 from './Pages/Error404.js'
+import './App.scss'
 
 
 /**
